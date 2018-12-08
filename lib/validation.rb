ca74b3d61c3a1074faa43ec471aaceec70928aa3
earl-stephens <stephensearl@hotmail.com>
2018-12-08 01:29:03 -0700

    updates to validation class and validation_test

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,13 +1,28 @@
 require './lib/board'
+require 'pry'
 
 class Validation < Board
+  attr_reader :split_coordinate_array,
+              :array_of_letters,
+              :array_of_numbers,
+              :array_of_ordinals,
+              :check_array
 
-  def valid_placement?(ship_arg, coordinate_arg)
-    valididate_ship_length_and_coordinate_length(ship_arg, coordinate_arg)
-    validate_consecutive_coordinates_when_placing_the_ship(ship_arg, coordinate_arg)
+  def initialize
+    @split_coordinate_array = []
+    @array_of_letters = []
+    @array_of_numbers = []
+    @array_of_ordinals = []
+    @check_array = []
   end
 
-  def valididate_ship_length_and_coordinate_length(ship_arg, coordinate_arg)
+  def valid_placement?(ship_arg, coordinate_arg)
+    valididate_ship_length_and_coordinate_length?(ship_arg, coordinate_arg)
+    break_down_the_coordinate_array(ship_arg, coordinate_arg)
+    validate_consecutive_coordinates_when_placing_the_ship?(ship_arg, coordinate_arg)
+  end
+
+  def valididate_ship_length_and_coordinate_length?(ship_arg, coordinate_arg)
     if ship_arg.length == coordinate_arg.length
       true
     else
@@ -15,8 +30,85 @@ class Validation < Board
     end
   end
 
-  def validate_consecutive_coordinates_when_placing_the_ship(ship_arg, coordinate_arg)
-    false
+  def break_down_the_coordinate_array(ship_arg, coordinate_arg)
+    split_the_coordinate_array(coordinate_arg)
+    populate_the_array_of_letters
+    populate_the_array_of_numbers
+    populate_the_check_array(ship_arg)
+  end
+
+  def split_the_coordinate_array(coordinate_arg)
+    coordinate_arg.each do |coordinate|
+      @split_coordinate_array << coordinate.split(//)
+    end
+    return @split_coordinate_array
+  end
+
+  def populate_the_array_of_letters
+    @split_coordinate_array.each do |element|
+      @array_of_letters << element[0]
+    end
+    @array_of_letters = @array_of_letters.sort
+    return @array_of_letters
+  end
+
+  def populate_the_array_of_numbers
+    @split_coordinate_array.each do |element|
+      @array_of_numbers << element[1].to_i
+    end
+    @array_of_numbers = @array_of_numbers.sort
+    return @array_of_numbers
+  end
+
+  def populate_the_check_array(ship_arg)
+    # binding.pry
+    start_range_number = @array_of_letters[0].ord
+    end_range_number = start_range_number + (ship_arg.length - 1)
+    range = start_range_number..end_range_number
+    @check_array = range.to_a
+    return @check_array
+
+  end
+
+  def validate_consecutive_coordinates_when_placing_the_ship?(ship_arg, coordinate_arg)
+    check_for_consecutive_vertical
+    check_for_consecutive_horizontal(ship_arg)
+    # binding.pry
+    if check_for_consecutive_vertical == true && check_for_consecutive_horizontal(ship_arg) == true
+      return false
+    end
+    if check_for_consecutive_vertical == false && check_for_consecutive_horizontal(ship_arg) == false
+      return false
+    end
+    if check_for_consecutive_vertical == true || check_for_consecutive_horizontal(ship_arg) == true
+      return true
+    end
+  end
+
+  def check_for_consecutive_vertical
+    temp_array = []
+    @array_of_letters.each do |element|
+      temp_array << element.ord
+    end
+    if temp_array == @check_array
+      true
+    else
+      false
+    end
+  end
+
+  def check_for_consecutive_horizontal(ship_arg)
+    start_range_number = @array_of_numbers[0]
+    end_range_number = start_range_number + ship_arg.length - 1
+    range = start_range_number..end_range_number
+    temp_array = []
+    temp_array = range.to_a
+    # binding.pry
+    if temp_array == @array_of_numbers
+      true
+    else
+      false
+    end
   end
 
 end
--- test/validation_test.rb
+++ test/validation_test.rb
@@ -9,11 +9,13 @@ require 'pry'
 class ValidationTest < Minitest::Test
 
   def test_it_exists
+    # skip
     valid_object = Validation.new
   assert_instance_of Validation, valid_object
   end
 
 def test_number_of_coordinates_is_same_as_ship_length
+  # skip
   cruiser = Ship.new("Cruiser", 3)
   board = Board.new
   board.cells
@@ -21,10 +23,11 @@ def test_number_of_coordinates_is_same_as_ship_length
 
   valid_object.valid_placement?(cruiser, ["C1", "C2", "C3"])
 
-  assert_equal true, valid_object.valididate_ship_length_and_coordinate_length(cruiser, ["C1", "C2", "C3"])
+  assert_equal true, valid_object.valididate_ship_length_and_coordinate_length?(cruiser, ["C1", "C2", "C3"])
 end
 
 def test_number_of_coordinates_is_same_as_ship_length_for_false_assertion
+  # skip
   submarine = Ship.new("Submarine", 2)
   board = Board.new
   board.cells
@@ -32,18 +35,140 @@ def test_number_of_coordinates_is_same_as_ship_length_for_false_assertion
 
   valid_object.valid_placement?(submarine, ["B2", "C2", "D2"])
 
-  assert_equal false, valid_object.valid_placement?(submarine, ["B2", "C2", "D2"])
+  assert_equal false, valid_object.valididate_ship_length_and_coordinate_length?(submarine, ["B2", "C2", "D2"])
 end
 
-def test_for_consecutive_coordinates
+def test_for_consecutive_coordinates_sad_path
+  # skip
   submarine = Ship.new("Submarine", 2)
   board = Board.new
   board.cells
   valid_object = Validation.new
 
-  valid_object.valid_placement?(submarine, ["B2", "C3"])
+  valid_object.valid_placement?(submarine, ["A1", "C1"])
 
-  assert_equal false, valid_object.validate_consecutive_coordinates_when_placing_the_ship(submarine, ["B2", "C3"])
+  assert_equal false, valid_object.validate_consecutive_coordinates_when_placing_the_ship?(submarine, ["A1", "C1"])
+end
+
+def test_for_consecutive_coordinates_happy_path
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["A1", "B1", "C1"])
+
+  assert_equal true, valid_object.validate_consecutive_coordinates_when_placing_the_ship?(cruiser, ["A1", "B1", "C1"])
+end
+
+def test_for_consecutive_coordinates_diagonal
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["A1", "B2", "C3"])
+
+  assert_equal false, valid_object.validate_consecutive_coordinates_when_placing_the_ship?(cruiser, ["A1", "B2", "C3"])
+end
+
+def test_for_splitting_the_coordinate_array
+  # skip
+  submarine = Ship.new("Submarine", 2)
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B2", "B3", "B4"])
+
+  assert_equal [["B", "2"], ["B", "3"], ["B", "4"]], valid_object.split_coordinate_array
+end
+
+def test_for_populating_the_array_of_letters
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["C2", "B2", "A2"])
+
+  assert_equal ["A", "B", "C"], valid_object.array_of_letters
+end
+
+def test_for_populating_the_array_of_numbers
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B4", "B3", "B2"])
+
+  assert_equal [2, 3, 4], valid_object.array_of_numbers
+end
+
+def test_for_populating_the_check_array
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["C4", "B4", "D4"])
+
+  assert_equal [66, 67, 68], valid_object.check_array
+end
+
+def test_for_consecutive_horizontal_coordinates_happy_path
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B4", "B3", "B2"])
+
+  assert_equal true, valid_object.check_for_consecutive_horizontal(cruiser)
+end
+
+def test_for_consecutive_horizontal_coordinates_sad_path
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B4", "B3", "B1"])
+
+  assert_equal false, valid_object.check_for_consecutive_horizontal(cruiser)
+end
+
+def test_for_consecutive_vertical_coordinates_happy_path
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B4", "C4", "A4"])
+
+  assert_equal true, valid_object.check_for_consecutive_vertical
+end
+
+def test_for_consecutive_vertical_coordinates_sad_path
+  # skip
+  cruiser = Ship.new("Cruiser", 3)
+  board = Board.new
+  board.cells
+  valid_object = Validation.new
+
+  valid_object.valid_placement?(cruiser, ["B4", "D4", "A4"])
+
+  assert_equal false, valid_object.check_for_consecutive_vertical
 end
 
 end
