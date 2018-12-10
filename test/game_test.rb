fc75ac73ccd7e728c8fe849430e4f34933d3e608
Erin King <eohone@gmail.com>
2018-12-10 08:52:50 -0700

    Added game.rb and game_test.rb

---
+++ test/game_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+require './lib/validation'
+require './lib/player'
+require './lib/game'
+require 'pry'
+
+class GameTest < Minitest::Test
+
+end
